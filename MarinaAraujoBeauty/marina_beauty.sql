eb588508d709fc457adb4cdc64567d416cb5708b
Bruno Pereira da Silva <54722109+Bruno491@users.noreply.github.com>
2024-11-24 20:34:27 -0300

    Upload do projeto php do EAD

---
+++ MarinaAraujoBeauty/marina_beauty.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE marina_beauty;
+
+USE marina_beauty;
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone VARCHAR(15) NOT NULL UNIQUE
+);
+
+CREATE TABLE agendamentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    data_atendimento DATE NOT NULL,
+    horario_atendimento TIME NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
+
+INSERT INTO clientes (nome, email, telefone) VALUES
+('João Silva', 'joao@gmail.com', '18991345896'),
+('Maria Oliveira', 'maria@gmail.com', '18994402892'),
+('Pedro Souza', 'pedro@gmail.com', '18993256851');
+
+INSERT INTO agendamentos (cliente_id, data_atendimento, horario_atendimento) VALUES
+(1, '2024-12-06', '15:00'),
+(2, '2024-11-25', '17:00'),
+(3, '2024-01-07', '18:00');
